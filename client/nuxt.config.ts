3729efc1e2b3b26e888ce4de72033fe827dbddc9
bktphan <bktphan.me@gmail.com>
2025-12-18 09:35:21 +0700

    chore: Configuration 'nuxt.config.ts' to enable module, TypeScript and TailwindCSS settings

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+  ],
+
+  typescript: {
+    typeCheck: true,
+    strict: true
+  },
+
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    viewer: true,
+  },
 })
