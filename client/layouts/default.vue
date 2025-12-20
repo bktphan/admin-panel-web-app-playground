ff3124321ec4f29f5204a05b1a4afbd0161f3e3f
bktphan <bktphan.me@gmail.com>
2025-12-20 23:35:37 +0700

    feat: Add the 'default.vue' file for main layout

---
+++ client/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen bg-gray-50 font-sans text-gray-900">
+    <AppSidebar />
+
+    <main class="md:ml-64 p-8 transition-all duration-300">
+      <slot />
+    </main>
+  </div>
+</template>
